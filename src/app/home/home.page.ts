c7d2b9d95443e6f08a3dc448962e7225775f6630
saqib92 <najam1992@gmail.com>
2021-07-02 13:15:00 +0500

    slide issue fix

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,32 +5,45 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
   imgs = [
     {
       img: 'assets/imgs/img.jpg',
-      tags:[]
+      tags: []
     },
     {
       img: 'assets/imgs/img-2.jpg',
-      tags:[]
+      tags: []
     }
   ];
 
-  constructor() {}
+  constructor() { }
 
+  // Old Function but issue on 2nd slide and responsive image
 
-  imgClick(event, i){
+  /*imgClick(event, i){
     console.log(event, i);
     let x = event.layerX;
     let y = event.layerY;
     this.imgs[i].tags.push({x: x, y: y, input:''})
-    
     console.log(this.imgs);
+  }*/
+
+  // New Function works with 2 slide but have position issues
+  imgClick(event, i) {
+    let offsetLeft = 0;
+    let offsetTop = 0;
+    let el = event.srcElement;
+    offsetLeft += el.offsetLeft;
+    offsetTop += el.offsetTop;
+    var relativeX = event.layerX - offsetLeft;
+    var relativeY = event.layerY - offsetTop;
+    this.imgs[i].tags.push({ x: relativeX, y: relativeY, input: '' });
   }
 
-  removeTagbox(ti, i){
+  removeTagbox(ti, i) {
     this.imgs[i].tags.splice(ti, 1)
   }
 
